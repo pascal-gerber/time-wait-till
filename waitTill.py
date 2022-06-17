a5a54bf0dc5bbf02095c9ea50c5d8e39938ef618
pascal-gerber <77777914+pascal-gerber@users.noreply.github.com>
2022-06-17 09:39:29 +0200

    Upgrade of Programm
    + Licence
    + small update to code
    + error handling
    + Read Me

--- waitTill.py
+++ waitTill.py
@@ -10,16 +10,32 @@ def waituntill(Time):
         now -= 86400
 
     Cuttime = Time.split(":")
+
+    Error = False
     
-    if len(Cuttime) == 3:
-        WaitTime = int(Cuttime[0]) * 3600 + int(Cuttime[1]) * 60 + int(Cuttime[2])
-    elif len(Cuttime) == 2:
+    if len(Cuttime) == 1: #do your own time in seconds directly (you have to calculate manually)
+        WaitTime = int(Cuttime[0])
+    elif len(Cuttime) == 2: #takes hours and minutes like "12:30", waits till "12:30"
         WaitTime = int(Cuttime[0]) * 3600 + int(Cuttime[1]) * 60
-    elif len(Cuttime) == 1:
-        WaitTime = int(Cuttime[2])
-    elif len(Cuttime) == 4:
+    elif len(Cuttime) == 3: #takes hours, minutes and seconds "12:30:10", waits till "12:30:10"
+        WaitTime = int(Cuttime[0]) * 3600 + int(Cuttime[1]) * 60 + int(Cuttime[2])
+    elif len(Cuttime) == 4: #takes days with specific commands on choice "1:12:30:00", waits till tomorow at "12:30"
         WaitTime = int(Cuttime[0]) * 86400 + int(Cuttime[1]) * 3600 + int(Cuttime[2]) * 60 + int(Cuttime[3])
-    
-    return(WaitTime - now)
-    
-             
+    elif len(Cuttime) == 0:
+        Error = True
+        raise TypeError("Expected a time")
+    else:
+        Error = True
+        raise TypeError("More than 4 options detected, did you mean:" +
+                        Cuttime[-3] + ":" + Cuttime[-2] + ":" + Cuttime[-1] + "?")
+
+    if Error == False:
+        return(WaitTime - now)
+
+#note: i programmed it purposefully so it can take way more than just example 60 seconds,
+#you can add as many seconds you want. 
+#it takes the beginning of today and adds all the userOptions to the start of the day in seconds.
+#Note: if theres any issue, contact me about it.
+
+#Made by Pascal
+            
